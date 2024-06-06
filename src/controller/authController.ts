b82e7af2d2b767dc13ba513fae689aeec11a445a
rashi9551 <rashidroz1361@gmail.com>
2024-06-06 20:54:23 +0530

    go auth services added

---
+++ src/controller/authController.ts
@@ -0,0 +1,39 @@
+import jwt, { Secret } from 'jsonwebtoken'
+import "dotenv/config"
+export class Authcontroller {
+
+
+    isAuthenticated = async (call:any, callback:any) => {
+        try{
+            console.log("its coming heare");
+            const token = call.request.token || '';
+            const decoded: any = jwt.verify(token, process.env.ACCESS_TOKEN || "Rashid" as Secret)
+            if(!decoded){
+                throw new Error('Invalid token')
+            }
+            callback(null,{userId : decoded.id, role: decoded.role})
+        }catch(e: any){
+            callback(e, null)
+        }
+    }
+
+    verifyToken = async(call:any, callback:any) => {
+        try{
+            const refreshtoken = call.request.token as string;
+            const decoded: any = jwt.verify(refreshtoken, process.env.REFRESH_TOKEN as Secret);
+            if(!decoded){
+                throw new Error("invalid token")
+            }
+            const refreshToken = jwt.sign({id: decoded.id, role: decoded.role}, process.env.REFRESH_TOKEN as Secret, {
+                expiresIn: "5m"
+            })
+            const accessToken = jwt.sign({id: decoded.id, role: decoded.role}, process.env.ACCESS_TOKEN as Secret, {
+                expiresIn: "3d"
+            })
+            const response = {accessToken, refreshToken}
+            callback(null, response)
+        }catch(e:any){
+            callback(e, null)
+        }
+    }
+}
---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import path from "path"
+import * as grpc from "@grpc/grpc-js"
+import * as protoLoader from "@grpc/proto-loader" 
+import "dotenv/config"
+import { Authcontroller } from "./controller/authController";
+
+
+const packageDef = protoLoader.loadSync(path.resolve(__dirname, './protos/auth.proto'))
+const grpcObject = (grpc.loadPackageDefinition(packageDef) as unknown) as any
+const authpackage = grpcObject.authpackage
+
+
+
+const controller = new Authcontroller()
+
+const server = new grpc.Server()
+
+const grpcServer = () => {
+    const port = process.env.PORT || '3001';
+    server.bindAsync(`localhost:${port}`,
+        grpc.ServerCredentials.createInsecure(),
+        (err, boundPort) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('grpc-auth server started on port', boundPort);
+        }
+    );
+}
+
+
+server.addService(authpackage.Auth.service, {
+    IsAuthenticated : controller.isAuthenticated,   
+    RefreshToken: controller.verifyToken
+
+    }
+)
+
+grpcServer();
