c667ce1cbddfb4279a66b0b7e3577d63bb652d8c
rashi9551 <rashidroz1361@gmail.com>
2024-07-15 03:54:25 +0530

    ci/cd checking

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import * as grpc from "@grpc/grpc-js"
 import * as protoLoader from "@grpc/proto-loader" 
 import "dotenv/config"
 import { Authcontroller } from "./controller/authController";
-
+import express from 'express'
 
 const packageDef = protoLoader.loadSync(path.resolve(__dirname, './protos/auth.proto'))
 const grpcObject = (grpc.loadPackageDefinition(packageDef) as unknown) as any
@@ -16,10 +16,11 @@ const controller = new Authcontroller()
 const server = new grpc.Server()
 
 const grpcServer = () => {
-    const port = process.env.AUTH_PORT || '3001';
+    const port = process.env.AUTH_GRPC_PORT || '3001';
     const Domain=process.env.NODE_ENV==='dev'?process.env.DEV_DOMAIN:process.env.PRO_DOMAIN_AUTH
     console.log(Domain)
-    server.bindAsync(`${Domain}:${port}`,
+    console.log(`0.0.0.0:${port}=-=-=-==-==-==`)
+    server.bindAsync(`0.0.0.0:${port}`,
         grpc.ServerCredentials.createInsecure(),
         (err, boundPort) => {
             if (err) {
@@ -31,11 +32,9 @@ const grpcServer = () => {
     );
 }
 
-
 server.addService(authpackage.Auth.service, {
     IsAuthenticated : controller.isAuthenticated,   
     RefreshToken: controller.verifyToken
-
     }
 )
 
